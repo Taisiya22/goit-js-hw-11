44614ac71aea9b5e3581810122f6dd5851d40c96
Taisiya Tomashevska <taya22011985@gmail.com>
2023-01-31 19:52:13 +0200

    3 com

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import Notiflix from "notiflix";
 
 
 const form = document.getElementById('search-form');
-const gallery = document.querySelector('.gallery')
+const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
-
-
+const API_KEY = '33166169-8998641aed83cb2aacc3c64c9';
 
   function getImg(name, page = 1) { 
-    const res = axios.get(`https://pixabay.com/api/?key=33166169-8998641aed83cb2aacc3c64c9&
-    q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+    const res = axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
      return res;
     
 }
@@ -29,7 +28,7 @@ function renderMarkup(pictures) {
    const markup = pictures.reduce((acc, { webformatURL, largeImageURL, likes, views, comments, downloads }) => {
         return acc + `
     <div class="photo-card">
-  <img src="${webformatURL}" alt="" loading="lazy" width = '200'/>
+  <img src="${webformatURL}" alt="" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b><span>${likes}</span>
