96b854b0fa000e532f079fae28c73922ef3c1e8b
Taisiya Tomashevska <taya22011985@gmail.com>
2023-02-02 16:05:55 +0200

    +saync

--- src/api.js
+++ src/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const URL = 'https://pixabay.com/api/';
 const API_KEY = '33166169-8998641aed83cb2aacc3c64c9';
 
-export function getImg(name, page = 1, per_page = 40) { 
+export async function getImg(name, page = 1, per_page = 40) { 
   const res = axios.get(`${URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
-  return res;
+  return await res;
     
 }
 
